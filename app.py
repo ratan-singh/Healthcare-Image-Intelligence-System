8445fad8f90187f21afa43c58554f5b037e418e4
ratan-singh <ratansingh_21515@aitpune.edu.in>
2025-08-09 11:05:00 +0530

    Integrate Google Generative AI for medical image analysis

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+import google.generativeai as genai
+import os
+from PIL import Image
+from dotenv import load_dotenv
+from flask import Flask, request, render_template
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model = genai.GenerativeModel("models/gemini-2.0-flash")
 
 app = Flask(__name__)
 
-@app.route('/')
+def gen_image(prompt, image):
+    response = model.generate_content([prompt, image])
+    return response.text
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Medical Image Recognition System - Coming Soon"
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        image = Image.open(uploaded_file)
+        prompt = 'Generate a medical description for this image.'
+        result = gen_image(prompt, image)
+        return render_template('index.html', response_text=result)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
